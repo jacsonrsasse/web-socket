83056e250f6668c9da72a1942f7539b485c4f8b4
Jacson R. Sasse <jacson.sasse@hotmail.com>
2023-11-26 01:29:51 -0300

    Feat: created enum to represent the events

---
+++ src/modules/tic-tac-toe/enums/tic-tac-toe-events.enum.ts
@@ -0,0 +1,6 @@
+export enum TicTacToeEvents {
+  CREATE_USER_ROOM = "create_user_room",
+  DEFINE_AS_SERVER = "define_as_server",
+  DISCONNECT_SOCKET = "disconnect_socket",
+  RELATE_USER_ID = "relate_user_id",
+}
