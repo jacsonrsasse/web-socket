cc4c970d3935611d5b912fe486b5751ad5e67f8e
Jacson R. Sasse <jacson.sasse@hotmail.com>
2023-11-26 12:45:18 -0300

    Feat: created functions to organize

--- src/modules/tic-tac-toe/tic-tac-toe.system.ts
+++ src/modules/tic-tac-toe/tic-tac-toe.system.ts
@@ -11,54 +11,60 @@ export default class TicTacToeSystem implements SystemInterface {
   private rooms: Array<string> = [];
 
   handler(socket: Socket) {
-    socket.on(TicTacToeServerSocketEvents.DEFINE_AS_SERVER, () => {
-      if (this.serverSocket) {
-        console.log("Ja tem um conectado");
-        // todo: implement change server connection if a new one income
-        return;
-      }
-      socket.leave(LOBBY_ROOM);
-
-      socket.removeAllListeners(TicTacToeServerSocketEvents.CREATE_USER_ROOM);
-
-      socket.join(SERVER_ROOM);
-
-      socket.on(
-        TicTacToeServerSocketEvents.DISCONNECT_CLIENT_SOCKET,
-        (event) => {
-          socket.nsp.in(event.body).disconnectSockets();
-          socket.emit(TicTacToeClientSocketEvents.CLIENT_SOCKET_DISCONNECTED);
-        }
-      );
-
-      socket.emit(TicTacToeClientSocketEvents.DEFINED_AS_SERVER);
-      this.serverSocket = socket;
-    });
+    socket.on(TicTacToeServerSocketEvents.DEFINE_AS_SERVER, () =>
+      this.defineAsServerHandler(socket)
+    );
 
     socket.join(LOBBY_ROOM);
 
-    socket.on(TicTacToeServerSocketEvents.CREATE_USER_ROOM, ({ body }) => {
-      try {
-        if (!this.serverSocket)
-          return socket.emit(TicTacToeClientSocketEvents.SERVER_OFFLINE);
+    socket.on(TicTacToeServerSocketEvents.CREATE_USER_ROOM, ({ body }) =>
+      this.createUserRoomHandler(socket, body)
+    );
 
-        socket.join(body);
-        this.rooms.push(body);
+    socket.on(TicTacToeServerSocketEvents.RELATE_USER_ID, ({ body }) =>
+      this.relateUserIdHandler(body)
+    );
+  }
 
-        socket.emit(TicTacToeClientSocketEvents.CONNECTED_IN_ROOM, body);
-      } catch (error) {
-        socket.disconnect();
-        this.serverSocket.emit(TicTacToeClientSocketEvents.CREATE_ROOM_ERROR);
-      }
+  private defineAsServerHandler(socket: Socket) {
+    if (this.serverSocket) {
+      console.log("Ja tem um conectado");
+      // todo: implement change server connection if a new one income
+      return;
+    }
+    socket.leave(LOBBY_ROOM);
+
+    socket.removeAllListeners(TicTacToeServerSocketEvents.CREATE_USER_ROOM);
+
+    socket.join(SERVER_ROOM);
+
+    socket.on(TicTacToeServerSocketEvents.DISCONNECT_CLIENT_SOCKET, (event) => {
+      socket.nsp.in(event.body).disconnectSockets();
+      socket.emit(TicTacToeClientSocketEvents.CLIENT_SOCKET_DISCONNECTED);
     });
 
-    socket.on(TicTacToeServerSocketEvents.RELATE_USER_ID, ({ body }) => {
-      try {
-        this.serverSocket.emit(
-          TicTacToeClientSocketEvents.RELATE_USER_ID,
-          body
-        );
-      } catch (error) {}
-    });
+    socket.emit(TicTacToeClientSocketEvents.DEFINED_AS_SERVER);
+    this.serverSocket = socket;
+  }
+
+  private createUserRoomHandler(socket: Socket, body: string) {
+    try {
+      if (!this.serverSocket)
+        return socket.emit(TicTacToeClientSocketEvents.SERVER_OFFLINE);
+
+      socket.join(body);
+      this.rooms.push(body);
+
+      socket.emit(TicTacToeClientSocketEvents.CONNECTED_IN_ROOM, body);
+    } catch (error) {
+      socket.disconnect();
+      this.serverSocket.emit(TicTacToeClientSocketEvents.CREATE_ROOM_ERROR);
+    }
+  }
+
+  private relateUserIdHandler(body: string) {
+    try {
+      this.serverSocket.emit(TicTacToeClientSocketEvents.RELATE_USER_ID, body);
+    } catch (error) {}
   }
 }
